d4822b0317abf71e42a19e9c9af6aee35b622111
Paulo Henrique de Siqueira <paulohenriqu@hotmail.com>
2018-11-28 09:00:30 -0200

    [PHS] - Código

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,9 @@ import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
+import { Camera } from '@ionic-native/camera';
+import { FileTransfer, FileTransferObject } from '@ionic-native/file-transfer';
+
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
@@ -24,6 +27,9 @@ import { HomePage } from '../pages/home/home';
   providers: [
     StatusBar,
     SplashScreen,
+    Camera,
+    FileTransfer,
+    FileTransferObject,   
     {provide: ErrorHandler, useClass: IonicErrorHandler}
   ]
 })
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,73 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { Component } from "@angular/core";
+import { NavController } from "ionic-angular";
+import { Camera } from "@ionic-native/camera";
+import { FileTransfer, FileTransferObject, FileUploadOptions } from "@ionic-native/file-transfer";
 
 @Component({
-  selector: 'page-home',
-  templateUrl: 'home.html'
+  selector: "page-home",
+  templateUrl: "home.html"
 })
 export class HomePage {
+  imageURI: string;
+  convertedURI: string;
+  private win: any = window;
 
-  constructor(public navCtrl: NavController) {
+  constructor(
+    public navCtrl: NavController,
+    public camera: Camera,
+    private transfer: FileTransfer
+  ) {}
 
+  foto() {
+    this.camera
+      .getPicture({
+        quality: 50,
+        destinationType: this.camera.DestinationType.FILE_URI,
+        sourceType: this.camera.PictureSourceType.CAMERA,
+        encodingType: this.camera.EncodingType.JPEG,
+        correctOrientation: true
+      })
+      .then(
+        imageData => {
+          this.imageURI = imageData;
+          this.convertedURI = this.win.Ionic.WebView.convertFileSrc(
+            this.imageURI
+          );
+          console.log(this.convertedURI);
+        },
+        err => {
+          console.log(err);
+        }
+      );
   }
 
+  uploadFile() {
+
+    console.log("Upload");
+    const fileTransfer: FileTransferObject = this.transfer.create();
+
+    let options: FileUploadOptions = {
+      fileKey: "file",
+      fileName: "ionicfile",
+      chunkedMode: false,
+      mimeType: "image/jpeg",
+      headers: {}
+    };
+
+    var params:any = {};
+    params.nome = "testeNome";
+    params.container = "abc";
+    options.params = params;
+
+    fileTransfer
+      .upload(this.imageURI, "http://192.168.0.102:8080/api/upload", options)
+      .then(
+        data => {
+          console.log(data + " Uploaded Successfully");
+        },
+        err => {
+          console.log(err);
+        }
+      );
+  }
 }
